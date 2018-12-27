f21ccc545b3282e8944ead790f6658e793cd7d1b
Benas Bagdanovas <benas.bagdanovas@gmail.com>
2018-12-27 21:43:45 +0200

    Setup a dummy 'GET' endpoint.

--- project/app/Main.hs
+++ project/app/Main.hs
@@ -8,8 +8,12 @@ import Web.Scotty
 
 import Data.Text.Lazy.Encoding (decodeUtf8)
 
-main = scotty 3000 $
+main = scotty 3000 $ do
+
     post "/:word" $ do
         beam <- param "word"
         bd <- body
         html $ mconcat ["<h1>Scotty, ", beam, decodeUtf8 bd, " me up!</h1>"]
+
+    get "/:word" $ do
+        html "test"
